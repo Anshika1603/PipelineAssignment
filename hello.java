56623928807097c512c8bb2b80c6cbdb18474f2b
Anshika1603 <anshika.srivastava@knoldus.com>
2023-02-20 13:01:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import java.io.*;
-import java.lang.*;
-class hello{  
+class Hello{  
         public static void main(String args[]){  
          System.out.println("Hello Java");  
         }  
